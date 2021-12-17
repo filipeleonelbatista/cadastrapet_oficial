5704d41b6ab43e4cce784c1399be9a23f5f6f2ca
Filipe Batista <filipe.x@live.com>
2021-12-17 10:24:21 -0300

    adicionando firebase no web

--- web/src/services/firebase-config.js
+++ web/src/services/firebase-config.js
@@ -1,15 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCYOBJLPPn9rr1NBP-xfG2Dp7oT1ul10qI",
   authDomain: "cadastra-pet.firebaseapp.com",
+  databaseURL: "https://cadastra-pet-default-rtdb.firebaseio.com",
   projectId: "cadastra-pet",
   storageBucket: "cadastra-pet.appspot.com",
   messagingSenderId: "141547492119",
   appId: "1:141547492119:web:f9bd6caad05d4973951d52",
 };
 
-export const FirebaseApp = initializeApp(firebaseConfig);
-export const FirebaseAnalytics = getAnalytics(FirebaseApp);
+const app = initializeApp(firebaseConfig);
+export const authentication = getAuth(app);
