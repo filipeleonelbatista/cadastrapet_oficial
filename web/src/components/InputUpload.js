a8aee558d0dd61dceebcd36b06c73470a14fcbf7
Filipe Batista <filipe.x@live.com>
2022-04-24 15:24:11 -0300

    criando multiupload com preview e overlay preview

---
+++ web/src/components/InputUpload.js
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import styles from "../styles/components/InputUpload.module.css";
+
+function InputUpload({ id, label, onChange, value, ...rest }) {
+  const [file, setfile] = useState();
+  const [files, setfiles] = useState([]);
+  const [isShow, setIsShow] = useState(false);
+
+  const handleFilePreview = async (fileArray) => {
+    let files = Array.from(fileArray).map((file) => {
+      let reader = new FileReader();
+      return new Promise((resolve) => {
+        reader.onloadend = () => resolve({ file, data_url: reader.result });
+        reader.readAsDataURL(file);
+      });
+    });
+
+    let res = await Promise.all(files);
+    console.log(res);
+    setfiles(res);
+  };
+
+  function handleClickInput() {
+    document.getElementById(id).click();
+  }
+
+  function handlePreview(e) {
+    onChange(e);
+    handleFilePreview(e.target.files);
+  }
+
+  function handleShowModal(selectedFile) {
+    setfile(selectedFile);
+    setIsShow(true);
+  }
+
+  function handleCloseModal() {
+    setfile(null);
+    setIsShow(false);
+  }
+
+  return (
+    <>
+      {isShow && (
+        <button onClick={handleCloseModal} className={styles.overlay}>
+          <img
+            src={file.data_url}
+            alt=""
+            className={styles.overlayImageContainer}
+          />
+          <div className={styles.imageData}>{file.file.name}</div>
+        </button>
+      )}
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.inputContainer}>
+            {label && <label htmlFor={id}>{label}</label>}
+            <input
+              id={id}
+              value={value}
+              onChange={(e) => handlePreview(e)}
+              multiple
+              type="file"
+              {...rest}
+            />
+            <button onClick={handleClickInput} title={label}>
+              <FaPlus />
+            </button>
+          </div>
+          {files.length > 0 && (
+            <div className={styles.imagePreviewContainer}>
+              <p>Imagens selecionadas</p>
+              <div className={styles.imageList}>
+                {files.map((file) => (
+                  <button
+                    onClick={() => handleShowModal(file)}
+                    className={styles.imageContainer}
+                  >
+                    <div className={styles.imageContent}>
+                      <img alt="Imagem Selecionada" src={file.data_url} />
+                    </div>
+                    <p>{file.file.name}</p>
+                  </button>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default InputUpload;
