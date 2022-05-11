ebc29e081e2bd6fd0d22ec648370b2573ca9ffa5
Filipe Batista <filipe.x@live.com>
2022-05-11 12:17:53 -0300

    adicionado a carterinha do pet

--- web/src/pages/tutor/PetInfo.js
+++ web/src/pages/tutor/PetInfo.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { FaCamera, FaTrash } from "react-icons/fa";
+import QRCode from "react-qr-code";
 import { useLocation, useNavigate } from "react-router-dom";
+import logoImg from "../../assets/icon_white.png";
+import dogImg from "../../assets/images/pet.jpg";
 import BackButton from "../../components/BackButton";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
@@ -34,6 +37,8 @@ function PetInfo() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [file, setFile] = useState(null);
   const [isView, setIsView] = useState(false);
+
+  const [selectedNav, setSelectedNav] = useState("pet_info");
 
   const handleDeletePet = async () => {
     if (
@@ -187,141 +192,247 @@ function PetInfo() {
         </div>
       </div>
       <div className={styles.content}>
-        {isView ? (
-          <div className={styles.uploadButton}>
-            <div
-              style={{
-                background: `url(${selectedPet.avatar}) no-repeat center center`,
-                borderRadius: "50%",
-                width: "100%",
-                height: "100%",
-                backgroundSize: "cover",
-              }}
-            ></div>
-          </div>
-        ) : (
-          <label className={styles.uploadButton}>
-            <input
-              required
-              className={styles.uploadInput}
-              type="file"
-              accept="image/png, image/jpeg"
-              onChange={(e) => handleFilePreview(e)}
-            ></input>
-            {selectedImage ? (
-              <div
-                alt="Imagem Selecionada"
-                style={{
-                  background: `url(${selectedImage}) no-repeat center center`,
-                  borderRadius: "50%",
-                  width: "100%",
-                  height: "100%",
-                  backgroundSize: "cover",
-                }}
-              ></div>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            margin: "0.8rem 0",
+            gap: "0.8rem",
+          }}
+        >
+          <Button
+            transparent={!(selectedNav === "pet_info")}
+            onClick={() => setSelectedNav("pet_info")}
+          >
+            Informações do pet
+          </Button>
+          {isView && (
+            <Button
+              transparent={!(selectedNav === "pet_info_doc")}
+              onClick={() => setSelectedNav("pet_info_doc")}
+            >
+              Regitro Pet
+            </Button>
+          )}
+        </div>
+
+        {selectedNav === "pet_info" && (
+          <>
+            {isView ? (
+              <div className={styles.uploadButton}>
+                <div
+                  style={{
+                    background: `url(${selectedPet.avatar}) no-repeat center center`,
+                    borderRadius: "50%",
+                    width: "100%",
+                    height: "100%",
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </div>
             ) : (
-              <FaCamera />
+              <label className={styles.uploadButton}>
+                <input
+                  required
+                  className={styles.uploadInput}
+                  type="file"
+                  accept="image/png, image/jpeg"
+                  onChange={(e) => handleFilePreview(e)}
+                ></input>
+                {selectedImage ? (
+                  <div
+                    alt="Imagem Selecionada"
+                    style={{
+                      background: `url(${selectedImage}) no-repeat center center`,
+                      borderRadius: "50%",
+                      width: "100%",
+                      height: "100%",
+                      backgroundSize: "cover",
+                    }}
+                  ></div>
+                ) : (
+                  <FaCamera />
+                )}
+              </label>
             )}
-          </label>
+
+            <div className={styles.inputForm}>
+              <Input
+                disabled={isView}
+                required
+                id="nome"
+                label="Nome"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <Input
+                disabled={isView}
+                required
+                id="dt_nascimento"
+                label="Data de Nascimento"
+                value={birth_date}
+                onChange={(e) => setBirthDate(date(e.target.value))}
+              />
+              <Input
+                disabled={isView}
+                required
+                id="dt_adocao"
+                label="Data de Adoção"
+                value={adoption_date}
+                onChange={(e) => setAdoptionDate(date(e.target.value))}
+              />
+
+              <Input
+                disabled={isView}
+                required
+                id="tipo"
+                label="Tipo/Especie"
+                value={species}
+                onChange={(e) => setSpecies(e.target.value)}
+              />
+
+              <Input
+                disabled={isView}
+                required
+                id="raca"
+                label="Raça"
+                value={animal_race}
+                onChange={(e) => setAnimalRace(e.target.value)}
+              />
+
+              <Input
+                disabled={isView}
+                required
+                id="pelagem"
+                label="Pelagem/Cor"
+                value={pelage}
+                onChange={(e) => setPelage(e.target.value)}
+              />
+
+              <Input
+                disabled={isView}
+                required
+                id="sexo"
+                label="Sexo"
+                value={sex}
+                onChange={(e) => setSex(e.target.value)}
+              />
+
+              <Input
+                disabled={isView}
+                id="castrado"
+                label="Data de Castração"
+                value={castration}
+                onChange={(e) => setCastration(date(e.target.value))}
+              />
+
+              <Input
+                disabled={isView}
+                id="coleira"
+                label="Coleira"
+                value={pin_number}
+                onChange={(e) => setPinNumber(e.target.value)}
+              />
+            </div>
+          </>
         )}
 
-        <div className={styles.inputForm}>
-          <Input
-            disabled={isView}
-            required
-            id="nome"
-            label="Nome"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <Input
-            disabled={isView}
-            required
-            id="dt_nascimento"
-            label="Data de Nascimento"
-            value={birth_date}
-            onChange={(e) => setBirthDate(date(e.target.value))}
-          />
-          <Input
-            disabled={isView}
-            required
-            id="dt_adocao"
-            label="Data de Adoção"
-            value={adoption_date}
-            onChange={(e) => setAdoptionDate(date(e.target.value))}
-          />
-
-          <Input
-            disabled={isView}
-            required
-            id="tipo"
-            label="Tipo/Especie"
-            value={species}
-            onChange={(e) => setSpecies(e.target.value)}
-          />
-
-          <Input
-            disabled={isView}
-            required
-            id="raca"
-            label="Raça"
-            value={animal_race}
-            onChange={(e) => setAnimalRace(e.target.value)}
-          />
-
-          <Input
-            disabled={isView}
-            required
-            id="pelagem"
-            label="Pelagem/Cor"
-            value={pelage}
-            onChange={(e) => setPelage(e.target.value)}
-          />
-
-          <Input
-            disabled={isView}
-            required
-            id="sexo"
-            label="Sexo"
-            value={sex}
-            onChange={(e) => setSex(e.target.value)}
-          />
-
-          <Input
-            disabled={isView}
-            id="castrado"
-            label="Data de Castração"
-            value={castration}
-            onChange={(e) => setCastration(date(e.target.value))}
-          />
-
-          <Input
-            disabled={isView}
-            id="coleira"
-            label="Coleira"
-            value={pin_number}
-            onChange={(e) => setPinNumber(e.target.value)}
-          />
-        </div>
+        {selectedNav === "pet_info_doc" && (
+          <>
+            <div className={styles.border}>
+              <div className={styles.bgDocumentContent}>
+                <img src={logoImg} width={40} alt="Cadastrapet logo" />
+                <strong className={styles.documentTitle}>
+                  REGISTRO GERAL DE ANIMAIS - CADASTRAPET
+                </strong>
+              </div>
+              <div className={styles.petData}>
+                <div className={styles.imageFrame}>
+                  <img
+                    src={selectedPet.avatar}
+                    alt="Doguinho"
+                    style={{
+                      backgroundImage: `url(${dogImg})`,
+                      backgroundRepeat: "no-repeat",
+                      backgroundPosition: "center",
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                      borderRadius: "0.8rem",
+                    }}
+                  />
+                </div>
+                <div>
+                  <p>
+                    <b>Nome: </b> {selectedPet.name}
+                    <br />
+                    <b>Tipo: </b> {selectedPet.species}
+                    <br />
+                    <b>Raça: </b> {selectedPet.animal_race}
+                    <br />
+                    <b>Sexo: </b> {selectedPet.sex}
+                    <br />
+                    <b>Tutor: </b> {user.name}
+                    <small>
+                      <br /> {user.phone} - {user.email}
+                    </small>
+                  </p>
+                  <p>
+                    <b>IdPet: </b> {selectedPet.uid}
+                    <br />
+                    <b>Coleira: </b> {selectedPet.pin_number}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.border}>
+              <div className={styles.bgDocumentContent}>
+                <img src={logoImg} width={40} alt="Cadastrapet logo" />
+                <strong className={styles.documentTitle}>
+                  REGISTRO GERAL DE ANIMAIS - CADASTRAPET
+                </strong>
+              </div>
+              <div className={styles.petData}>
+                <div className={styles.qrCodeFrame}>
+                  <QRCode size={190} value={`${selectedPet.uid}`} />
+                </div>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <p style={{ textAlign: "center" }}>
+                    Passe o leitor de <b>QR CODE</b> do celular <br />
+                    Ou digite o código no site
+                    <br />
+                    <b>https://cadastrapet.com.br</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
-      {isView ? (
+      {selectedNav === "pet_info" && (
         <>
-          <Button id="nome" onClick={() => navigate("/tutor/petinfo/edit")}>
-            Editar
-          </Button>
-          <Button
-            onClick={handleDeletePet}
-            style={{ backgroundColor: "red", cursor: "pointer" }}
-          >
-            <FaTrash />
-            Deletar Registro
-          </Button>
+          {isView ? (
+            <>
+              <Button id="nome" onClick={() => navigate("/tutor/petinfo/edit")}>
+                Editar
+              </Button>
+              <Button
+                onClick={handleDeletePet}
+                style={{ backgroundColor: "red", cursor: "pointer" }}
+              >
+                <FaTrash />
+                Deletar Registro
+              </Button>
+            </>
+          ) : (
+            <Button id="nome" onClick={handlUpdatePet}>
+              Salvar
+            </Button>
+          )}
         </>
-      ) : (
-        <Button id="nome" onClick={handlUpdatePet}>
-          Salvar
-        </Button>
       )}
+
       <Version />
       <Widget />
     </div>
