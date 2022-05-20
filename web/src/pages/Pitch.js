374492e648839b85d54c6e46743b5edb3684ebbf
Filipe Batista <filipe.x@live.com>
2022-05-20 14:54:20 -0300

    removendo componente de pdf do pitch

--- web/src/pages/Pitch.js
+++ web/src/pages/Pitch.js
@@ -1,14 +1,12 @@
-import { Viewer, Worker } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/core/lib/styles/index.css";
 import React from "react";
 import styles from "../styles/pages/Pitch.module.css";
 
 export default function Pitch() {
   return (
-    <div className={styles.container}>
-      <Worker workerUrl="./js/pdf.worker.min.js">
-        <Viewer fileUrl="./images/pitch/Pitch.pdf" />
-      </Worker>
-    </div>
+    <object
+      className={styles.container}
+      type="application/pdf"
+      data="./images/pitch/Pitch.pdf"
+    ></object>
   );
 }
