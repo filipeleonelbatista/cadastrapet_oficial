66faede7aa0100307ac7c07020e329f245b5c99d
Filipe Batista <filipe.x@live.com>
2023-01-30 11:12:40 -0300

    ajustes v1 homolog

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -150,6 +150,8 @@ export function AuthContextProvider(props) {
       if (user) {
         setIsLoggedIn(true);
         setKeyLocalStorage("UID", user.uid);
+
+        console.log("USER", user)
         const loggedUser = await getUserByID(user.uid);
         let currentPetList = [];
 
@@ -331,14 +333,12 @@ export function AuthContextProvider(props) {
       return true;
     } catch (err) {
       sendDiscordNotification(
-        `Houve um erro ao ${
-          message ? "adicionar" : "atualizar dados do"
+        `Houve um erro ao ${message ? "adicionar" : "atualizar dados do"
         } pet\n\n \`${JSON.stringify(data)}\`\n\nlog do erro:\n\n\`${err}\``,
         "doguinho"
       );
       alert(
-        `Houve um erro ao ${
-          message ? "adicionar" : "atualizar dados do"
+        `Houve um erro ao ${message ? "adicionar" : "atualizar dados do"
         } pet. Tente novamente mais tarde`
       );
       return false;
