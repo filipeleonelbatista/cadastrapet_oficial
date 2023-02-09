7c7a35073cc6c45e1d77db53efa9f9b674fd4b11
Filipe Batista <filipe.x@live.com>
2023-02-09 07:35:57 -0300

    removendo pwa serviceworkers

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 
-import reportWebVitals from "./reportWebVitals";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
-serviceWorkerRegistration.register();
-
-reportWebVitals();
